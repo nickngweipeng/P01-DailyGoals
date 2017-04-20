7873376c159c3f1f3d36b2c7414cf0bb0959223b
nickngweipeng <15056201@myrp.edu.sg>
2017-04-20 15:59:28 +0800

    ok

--- app/src/main/java/com/myapplicationdev/android/p01_dailygoals/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p01_dailygoals/MainActivity.java
@@ -1,7 +1,13 @@
 package com.myapplicationdev.android.p01_dailygoals;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnDone = (Button) findViewById(R.id.btnOk);
+        btnDone.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View arg0) {
+                RadioGroup rg1 = (RadioGroup) findViewById(R.id.radioGroup1);
+                RadioGroup rg2 = (RadioGroup) findViewById(R.id.radioGroup2);
+                RadioGroup rg3 = (RadioGroup) findViewById(R.id.radioGroup3);
+                int selectedButtonId1 = rg1.getCheckedRadioButtonId();
+                int selectedButtonId2 = rg2.getCheckedRadioButtonId();
+                int selectedButtonId3 = rg3.getCheckedRadioButtonId();
+                RadioButton rb1 = (RadioButton) findViewById(selectedButtonId1);
+                RadioButton rb2 = (RadioButton) findViewById(selectedButtonId2);
+                RadioButton rb3 = (RadioButton) findViewById(selectedButtonId3);
+
+                EditText refText = (EditText) findViewById(R.id.reflectionText);
+
+                String[] info = {rb1.getText().toString(), rb2.getText().toString(), rb3.getText().toString(), refText.getText().toString()};
+                // Create an intent to start another activity called
+                //  DemoActivities (which we would create later)
+                Intent i = new Intent(MainActivity.this,
+                        summary.class);
+                // Pass the String array holding the name & age to new activity
+                i.putExtra("info", info);
+                // Start the new activity
+                startActivity(i);
+            }});
     }
 }
---
+++ app/src/main/java/com/myapplicationdev/android/p01_dailygoals/summary.java
@@ -0,0 +1,32 @@
+package com.myapplicationdev.android.p01_dailygoals;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class summary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+        // Get the intent so as to get the "things" inside the intent
+        Intent i = getIntent();
+        // Get the String array named "info" we passed in
+        String[] info = i.getStringArrayExtra("info");
+        // Get the TextView object
+        TextView tv1 = (TextView) findViewById(R.id.tvQn1);
+        TextView tv2 = (TextView) findViewById(R.id.tvQn2);
+        TextView tv3 = (TextView) findViewById(R.id.tvQn3);
+        TextView tv4 = (TextView) findViewById(R.id.tvQn4);
+        // Display the name and age on the TextView
+        tv1.setText(info[0]);
+        tv2.setText(info[1]);
+        tv3.setText(info[2]);
+        tv4.setText(info[3]);
+    }
+}
